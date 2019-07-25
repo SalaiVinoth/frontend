36ec5bc4c11628ca8be93965248b49ea4bea3414
SalaiVinoth <52716195+SalaiVinoth@users.noreply.github.com>
2019-07-25 22:00:07 +0530

    Update main.js

--- freshworks frontend/main.js
+++ freshworks frontend/main.js
@@ -4,29 +4,16 @@ const countriesList = document.getElementById("countries");
 let countries; // will contain "fetched" data
 
 // Event Listeners
-// countriesList.addEventListener("change", event => displayCountryInfo(event.target.value));
+
 
 countriesList.addEventListener("change", newCountrySelection);
 // triggers display function
 function newCountrySelection(event) {
   displayCountryInfo(event.target.value);
-  //alert(event.target.value);
+  
   countryinfo=event.target.value;
-  alert(countryinfo);
 }
 
-// fetch("https://restcountries.eu/rest/v2/all")
-// .then(function(res){
-//   // console.log(res);
-//   return res.json();
-// })
-// .then(function(data){
-//   // console.log(data);
-//   initialize(data);
-// })
-// .catch(function(err){
-//   console.log("Error:", err);
-// });
 //fetch call
 fetch("https://restcountries.eu/rest/v2/all")
 .then(res => res.json())
@@ -36,27 +23,16 @@ fetch("https://restcountries.eu/rest/v2/all")
 function initialize(countriesData) {
   countries = countriesData;
   let options = "";
-  // for(let i=0; i<countries.length; i++) {
-  //   options += `<option value="${countries[i].alpha3Code}">${countries[i].name}</option>`;
-  //   // options += `<option value="${countries[i].alpha3Code}">${countries[i].name} (+${countries[i].callingCodes[0]})</option>`;
-  // }
   countries.forEach(country => options+=`<option value="${country.alpha3Code}">${country.name}</option>`);
-  // document.getElementById("countries").innerHTML = options;
-  // document.querySelector("#countries").innerHTML = options;
+
   countriesList.innerHTML = options;
-  // console.log(countriesList);
-  // console.log(countriesList.value);
-  // console.log(countriesList.length);
-  // console.log(countriesList.selectedIndex);
-  // console.log(countriesList[10]);
-  // console.log(countriesList[10].value);
-  // console.log(countriesList[10].text);
+
   countriesList.selectedIndex = Math.floor(Math.random()*countriesList.length);
   displayCountryInfo(countriesList[countriesList.selectedIndex].value);
 }
 // display return for html
 function displayCountryInfo(countryByAlpha3Code) {
- // alert(countryByAlpha3Code);
+ 
   const countryData = countries.find(country => country.alpha3Code === countryByAlpha3Code);
   document.querySelector("#flag-container img").src = countryData.flag;
   document.querySelector("#flag-container img").alt = `Flag of ${countryData.name}`;  
@@ -72,12 +48,10 @@ function displayCountryInfo(countryByAlpha3Code) {
 function myfunc()
 {
 	window.location="sample.html";
-	alert(countryinfo);
 	sampleCountryInfo(countryinfo);
 }
 function sampleCountryInfo(countryByAlpha3)
 {
 	const countryData = countries.find(country => country.alpha3Code === countryByAlpha3);
-	alert(countryData.capital);
 	document.getElementById("country__output-name").innerText=countryData.capital;
 }
